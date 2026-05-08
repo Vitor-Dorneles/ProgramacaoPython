f674c93844263468ad38b7b1ba89dd9a4023adf9
Vitor-Dorneles <dorneles.vitor@ufn.edu.br>
2026-05-07 21:25:48 -0300

    Adição e Finalização Aula 12

---
+++ Aulas/Aula_12_dicionario/dicionario_exmplo.py
@@ -0,0 +1,20 @@
+# lista = [123,100,230]
+lista = []
+dicionario = { 
+    "placa" : "ittt",
+    "marca" : "Fiat",
+    "modelo" : "uno",
+    "ano" : 2025
+}
+lista.append(dicionario)
+
+dicionario = { 
+    "placa" : "i2334tt",
+    "marca" : "Fiat",
+    "modelo" : "toro",
+    "ano" : 2002
+}
+lista.append(dicionario)
+
+print(lista)
+# print(dicionario)
