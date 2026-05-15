e353f9d687715af562f2fb3350e82f3a965cb1ce
Vitor-Dorneles <dorneles.vitor@ufn.edu.br>
2026-05-14 21:35:49 -0300

    Finalização Exemplos aula

--- Aulas/Aula_13_POO/jodador.py
+++ Aulas/Aula_13_POO/jodador.py
@@ -13,18 +13,31 @@ def eh_maior_de_idade(self):
         return False
     
 def tem_telefone(self):
-    if self.sexo =='F' and self.eh_maior_de_idade():
+    if self.sexo =='Feminino' and self.eh_maior_de_idade():
         return self.telefone
     else:
         return None
 
-j1 = jogador('gabriel', 21, 'M')
-j2 = jogador('alisson', 19, 'M')
-j3 = jogador('Maria', 12, 'F')
+# j1 = jogador('gabriel', 21, 'M', 87878)
+# j2 = jogador('alisson', 19, 'M', 4545)
+# j3 = jogador('Maria', 12, 'F', 4545896 )
 
-lista_objetos = []
-lista_objetos.append(j1)
-lista_objetos.append(j2)
-lista_objetos.append(j3)
+# lista_objetos = []
+# lista_objetos.append(j1)
+# lista_objetos.append(j2)
+# lista_objetos.append(j3)
 
-print(j1.eh_maior_de_idade())
+# print(j1.eh_maior_de_idade())
+
+nome_arquivo = 'Aulas\Aula_13_POO\jogadores.csv'
+
+lista_jogadores = []
+
+with open(nome_arquivo, 'r') as arquivo:
+    for linha in arquivo:
+        nome,idade,sexo, telefone = linha.strip().split(',')
+        lista_jogadores.append((nome, int(idade), sexo, telefone))
+
+for jogador in lista_jogadores:
+    print(jogador)
+    print('-' * 20)
